78b25b5c9f93eb3368ed2707e8e6c351dd386772
Hz-36 <62854395+Hz-36@users.noreply.github.com>
2023-09-26 16:31:59 +0200

    Ajax Shell Request Dev Tool

---
+++ AjaxShellRequest.py
@@ -0,0 +1,19 @@
+import http.client
+
+def send_ajax_request(path):
+    conn = http.client.HTTPConnection("localhost", 4444)  # Adjust the host and port accordingly
+    conn.request("GET", path)
+    response = conn.getresponse()
+    print("Response status:", response.status)
+    print("Response reason:", response.reason)
+    print("Response body:", response.read().decode('utf-8'))
+
+if __name__ == "__main__":
+    # Example AJAX requests
+    send_ajax_request("/xEXECUTE_CMDx")
+    send_ajax_request("/xEXECUTE_CMDx")
+    send_ajax_request("/xSHELL_CLIENTx/XCLIENT00001")
+
+    input("Press Enter to exit...")
+
+    #send_ajax_request("/xSTART_LISTENERx/XCLIENT00002")

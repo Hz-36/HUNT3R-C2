7e7aee5c597e49fb999739ba7a1d9b4d6f962f96
Hz-36 <62854395+Hz-36@users.noreply.github.com>
2023-09-26 16:32:31 +0200

    Update AjaxShellRequest.py

--- AjaxShellRequest.py
+++ AjaxShellRequest.py
@@ -1,7 +1,7 @@
 import http.client
 
 def send_ajax_request(path):
-    conn = http.client.HTTPConnection("localhost", 4444)  # Adjust the host and port accordingly
+    conn = http.client.HTTPConnection("localhost", 4444) 
     conn.request("GET", path)
     response = conn.getresponse()
     print("Response status:", response.status)
@@ -9,7 +9,6 @@ def send_ajax_request(path):
     print("Response body:", response.read().decode('utf-8'))
 
 if __name__ == "__main__":
-    # Example AJAX requests
     send_ajax_request("/xEXECUTE_CMDx")
     send_ajax_request("/xEXECUTE_CMDx")
     send_ajax_request("/xSHELL_CLIENTx/XCLIENT00001")
